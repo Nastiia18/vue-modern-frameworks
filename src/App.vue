bfa1275c487f6619fa6fe6035baa8f69412baf43
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2025-04-09 13:52:04 +0300

    Vue project with Vuetify 1

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item link title="About" to="/about">
+          <template v-slot:prepend>
+            <v-icon>mdi-information</v-icon>
+          </template>
+        </v-list-item>
+
+        <v-list-item link title="Contents" to="/contents">
+          <template v-slot:prepend>
+            <v-icon>mdi-book-open-page-variant</v-icon>
+          </template>
+        </v-list-item>
+
+        <v-list-item link title="List Item 3">
+          <template v-slot:prepend>
+            <v-icon>mdi-format-list-checks</v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      color="teal-darken-4"
+      image="https://picsum.photos/1920/1080?random" fixed>
+
+      <template v-slot:image>
+        <v-img
+          gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+</script>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about pa-6">
+    <h1>About the project</h1>
+    <p>This project uses Vue.js and Vuetify to create a user-friendly and responsive interface.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
